e774109688b2ce1f48b40e0612a3d47ceae9aa79
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-23 16:42:54 +0800

    建立 TransactionService
    1.　加入建構式：TransactionService
    　1.1　sourceConnectionString：來源資料庫連線
    　1.2　targetConnectionString：目標資料庫連線
    
    2.　加入方法：MoveStudent

---
+++ Service/TransactionService.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Model;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Transactions;
+
+namespace Service
+{
+    public sealed class TransactionService
+    {
+        private readonly string sourceConnectionString;
+        private readonly string targetConnectionString;
+
+        public TransactionService(string sourceConnectionString, string targetConnectionString)
+        {
+            this.sourceConnectionString = sourceConnectionString;
+            this.targetConnectionString = targetConnectionString;
+        }
+
+        public bool MoveStudent(int studentId)
+        {
+            try
+            {
+                using (var scope = new TransactionScope())
+                {
+                    var student = new Student();
+
+                    using (var sqlConn = new SqlConnection(this.sourceConnectionString))
+                    {
+                        var students = sqlConn.Query<Student>(@"SELECT [Id], [Name] FROM [Student] WHERE [Id] = @Id", new { Id = studentId });
+
+                        if (students == null || students.Count() != 1)
+                        {
+                            return false;
+                        }
+
+                        student = students.Single();
+
+                        sqlConn.Execute(@"DELETE FROM [Student] WHERE [Id] = @Id AND [Name] = @Name", student);
+                    }
+
+                    using (var sqlConn = new SqlConnection(this.targetConnectionString))
+                    {
+                        sqlConn.Execute(@"INSERT INTO [Student]([Id], [Name]) VALUES (@Id, @Name)", student);
+                    }
+
+                    // 若在 complete 前發生例外，包含在 scope 內的交易將會被 roll back
+                    scope.Complete();
+
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
